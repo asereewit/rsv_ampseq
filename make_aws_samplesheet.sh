8bc50e186a5002eb019f7517b293a5a61625a000
jaydee <68099601+asereewit@users.noreply.github.com>
2023-05-12 15:47:41 -0700

    add bash script for making samplesheet.csv for files on aws

---
+++ make_aws_samplesheet.sh
@@ -0,0 +1,11 @@
+aws_dir=$1
+r1_ext='L001_R1_001.fastq.gz'
+r2_ext='L001_R2_001.fastq.gz'
+aws s3 ls ${aws_dir} | grep "fastq.gz" | cut -d " " -f7 > aws_file_list.txt
+echo "sample,fastq_1,fastq_2" > samplesheet.csv
+IFS=$'\n\r'
+for line in $(cat aws_file_list.txt | grep "${r1_ext}")
+do
+	base=`basename $line $r1_ext`
+	echo "${base},${aws_dir}${base}${r1_ext},${aws_dir}${base}${r2_ext}" >> samplesheet.csv 
+done
