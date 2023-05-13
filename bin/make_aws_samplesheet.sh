cdb1cedcefe54232bc1e01ff0779d95d71ada843
jaydee <68099601+asereewit@users.noreply.github.com>
2023-05-12 23:04:49 -0700

    change fastp parameters: --cut_mean_quality 20 --qualified_quality_phred 20 --unqualified_percent_limit 20

--- bin/make_aws_samplesheet.sh
+++ bin/make_aws_samplesheet.sh
@@ -1,11 +1,12 @@
 aws_dir=$1
-r1_ext='_L001_R1_001.fastq.gz'
-r2_ext='_L001_R2_001.fastq.gz'
+run_name=$2
+r1_ext=$3
+r2_ext=$4
 aws s3 ls ${aws_dir} | grep "fastq.gz" | rev | cut -d " " -f1 | rev > aws_file_list.txt
-echo "sample,fastq_1,fastq_2" > samplesheet.csv
+echo "sample,fastq_1,fastq_2" > ${run_name}_samplesheet.csv
 IFS=$'\n\r'
 for line in $(cat aws_file_list.txt | grep "${r1_ext}")
 do
 	base=`basename $line $r1_ext`
-	echo "${base},${aws_dir}${base}${r1_ext},${aws_dir}${base}${r2_ext}" >> samplesheet.csv 
+	echo "${base},${aws_dir}${base}${r1_ext},${aws_dir}${base}${r2_ext}" >> ${run_name}_samplesheet.csv 
 done
